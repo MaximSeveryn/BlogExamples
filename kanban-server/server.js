f0f829ecd9445d650bdb4239de01516c05d337d0
MaximSeveryn <severyn.maxim@gmail.com>
2023-01-05 13:34:06 +0200

    PORT -> port
    PORT -> port

--- kanban-server/server.js
+++ kanban-server/server.js
@@ -1,12 +1,12 @@
 const registerApp = require("./app");
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 require("dotenv").config();
 
 const main = async () => {
   const app = await registerApp();
 
-  app.listen(PORT, () => {
-    console.log(`Listening on Port ${PORT}\nGo to http://localhost:${PORT}`);
+  app.listen(port, () => {
+    console.log(`Listening on Port ${port}\nGo to http://localhost:${port}`);
   });
 };
 
